f9c3892a7f906780595f99b990d4449e7c1871da
jholmes16 <jholmesarea@gmail.com>
2022-03-25 12:41:18 -0400

    Add google login credentials

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
 # Set up os.environ varibles only when running locally #
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+os.environ['OATHLIB_RELAX_TOKEN_SCOPE'] = 'true'
 ########################################################
 from flask import Flask, redirect, url_for, render_template
 from flask_dance.contrib.google import make_google_blueprint, google
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SECRET_KEY'] = 'mysecret'
 
-blueprint = make_google_blueprint(client_id='', client_secret='', offline=True, scope=['profile', 'email'])
+blueprint = make_google_blueprint(client_id='must add credentials',
+                                  client_secret='must add credentials',
+                                  offline=True,
+                                  scope=["https://www.googleapis.com/auth/userinfo.email", "openid", "https://www.googleapis.com/auth/userinfo.profile"])
 
 app.register_blueprint(blueprint, url_prefix='/login')
 
@@ -20,19 +23,20 @@ def index():
 
 @app.route('/welcome')
 def welcome():
-    # Return Error Internal Server Error if not Logged In! #
-    resp = google.get('/oauth2/v2/userinfo')
+    resp = google.get('/oauth2/v3/userinfo')
     assert resp.ok, resp.text
-    email = resp.json['email']
-    
-    return render_template('welcome.html', email = email)
+    email = resp.json()['email']
+    return render_template('welcome.html', email=email)
 
 @app.route('/login/google')
 def login():
     if not google.authorized:
         return render_template(url_for('google.login'))
-    resp = google.get('/oauth2/v2/userinfo')
+    resp = google.get('/oauth2/v3/userinfo')
     assert resp.ok, resp.text
-    email = resp.json['email']
+    email = resp.json()['email']
+    return render_template('welcome.html', email=email)
 
-    return render_template('welcome.html', email = email)
+
+if __name__ == '__main__':
+        app.run()
